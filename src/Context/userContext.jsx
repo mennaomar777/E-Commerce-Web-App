8dfa780fddd7af78eeebafed571cd7a3b0ff9653
mennaomar777 <101195889+mennaomar777@users.noreply.github.com>
2025-05-29 23:40:07 +0300

    Update userContext.jsx

--- src/Context/userContext.jsx
+++ src/Context/userContext.jsx
@@ -3,11 +3,14 @@ import { createContext , useState ,useEffect } from "react";
 export let UserContext = createContext('')
 
 export default function UserContextProvider({children}){
-    const [token, settoken] = useState(localStorage.getItem('userToken'))
-    
-    
+    const [token, settoken] = useState(localStorage.getItem('userToken'))  
 
-   return <UserContext.Provider value={{token , settoken}}>
+      const setUserToken = (newToken) => {
+        localStorage.setItem('userToken', newToken);
+        settoken(newToken);
+    };
+
+   return <UserContext.Provider value={{token, settoken: setUserToken}}>
     {children}
    </UserContext.Provider>
 }
