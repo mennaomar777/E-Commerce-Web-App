5a02289dba0299d3ddfb38793648237656df1fc9
mennaomar777 <101195889+mennaomar777@users.noreply.github.com>
2025-06-13 00:40:40 +0300

    Update NotFound.jsx

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import style from './NotFound.module.css'
+import error from "../../assets/error.svg";
+
 export default function NotFound() {
   return (
     <>
        <div className='flex justify-center items-center flex-col w-full h-dvh'>
-        <h1 className='text-5xl font-bold'>4 0 4</h1>
-        <p className='text-slate-600 my-2'>Page Not Found !!!</p>
+        <img src={error} alt="" />
        </div>
     </>
   )
