788203fafc581070bfce088f583fafd6915b110c
mennaomar777 <101195889+mennaomar777@users.noreply.github.com>
2025-06-13 14:40:43 +0300

    Update AllOrders.jsx

--- src/components/AllOrders/AllOrders.jsx
+++ src/components/AllOrders/AllOrders.jsx
@@ -1,80 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import style from './AllOrders.module.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { FadeLoader } from 'react-spinners';
+
+
 
 export default function AllOrders() {
-  const headers = {
-    token: localStorage.getItem('userToken')
-  };
+      const [isLoading, setIsLoading] = useState(false);
+      const [message, setMessage] = useState(null);
+      const navigate = useNavigate();
   
-  const [userId, setUserId] = useState('679a68bbe75e72a76b173e2d');
-  const [allOrders, setAllOrders] = useState([]); 
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  async function getAllOrders(userId) {
-    try {
-      const res = await axios.get(
-        `https://ecommerce.routemisr.com/api/v1/orders/user/${userId}`,
-        { headers }
-      );
-      console.log(res.data);
-      setAllOrders(res.data); 
-      setLoading(true);
-    } catch (err) {
-      setError(err.message);
-    }
-    finally{
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    getAllOrders(userId);
-  }, [userId]); 
-
-  if (loading) return <div>Loading orders...</div>;
-  if (error) return <div>Error: {error}</div>;
-
   return (
     <>
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-5">
-        <div className='flex justify-between items-center'>
-          <div>
-            <h2 className='text-3xl font-bold p-5 text-green-600'>Your Orders</h2>
-          </div>
-        </div>
-        <table className="w-3/4 mx-auto my-6 text-sm text-left rtl:text-right text-gray-500">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Number of items
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Payment method
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Total price
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {allOrders?.map((order) => (
-              <tr key={order._id} className="bg-white border-b border-gray-200 hover:bg-gray-50">
-                <td className="px-6 py-4">
-                  {order.cartItems.length} items
-                </td>
-                <td className="px-6 py-4">
-                  {order.paymentMethodType}
-                </td>
-                <td className="px-6 py-4">
-                  {order.totalOrderPrice} EGP
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
+       {isLoading && (
+         <div className="w-full flex justify-center items-center bg-white bg-opacity-70 min-h-[70vh]">
+           <FadeLoader color="green" />
+         </div>
+       )}
+   
+       <div className="max-h-screen flex justify-center items-center py-4 my-5">
+         <div className="bg-white p-8 rounded-lg w-[350px] mx-4 max-w-md sm:max-w-md md:max-w-lg lg:max-w-xl border border-gray-200 shadow-lg mt-10">
+           <div className="text-center">
+             <div className="animate-bounce inline-flex justify-center items-center w-[50px] h-[50px] rounded-full bg-green-200 p-6 outline-8 outline-green-100/60 mb-3">
+               <i className="fa-solid fa-circle-check text-4xl text-green-600"></i>
+             </div>
+             <h2 className="text-2xl font-bold mb-1">Order Confirmed!</h2>
+             <p className="text-gray-600 mb-6 text-sm">
+              Thank you for shopping with us. We’re getting your order ready for delivery.
+             </p>
+           </div>
+   
+           {message && (
+             <p className="text-center mt-4 text-sm text-green-600">{message}</p>
+           )}
+           <div className="text-center my-2 ">
+             <Link to="/" className="text-center text-sm">
+               <div>
+                 <i className="fa-solid fa-arrow-left text-gray-500 "></i>{' '}
+                 <span className="text-gray-500">Back to Home</span>
+               </div>
+             </Link>
+           </div>
+         </div>
+       </div>
+     </>
   );
 }
